86f69fa264e4b893da943ad1e26b4c5471a56df1
jbrightuniverse <30967260+jbrightuniverse@users.noreply.github.com>
2021-04-13 00:59:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url='https://github.com/jbrightuniverse/hungarianviz',
     author='James Yuming Yu',
     packages=['hungarianviz'],
-    install_requires=['numpy', 'PIL'],
+    install_requires=['numpy', 'Pillow'],
     version='0.0.1',
     license='MIT',
     description='Implementation of the Hungarian Algorithm for optimal matching in bipartite weighted graphs.',
